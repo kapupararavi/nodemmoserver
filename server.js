7d7c9e2c1e4daa4612b30a9d76043fa8193e5efd
Ravi <ketan.ramani@kaprat.com>
2018-09-21 15:55:59 +0530

    Test change

--- server.js
+++ server.js
@@ -11,12 +11,6 @@ var clients = [];
 
 
 
-var server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080;
-var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
- 
-server.listen(server_port, server_ip_address, function () {
-  console.log( "Listening on " + server_ip_address + ", port " + server_port );
-});
 
 
 app.get('/', function(req, res) {
