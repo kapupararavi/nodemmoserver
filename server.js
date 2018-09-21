2c1d34ee5959cf7e758de71e696a68d3dbc14df1
Ravi <ketan.ramani@kaprat.com>
2018-09-21 15:51:33 +0530

    Test change

--- server.js
+++ server.js
@@ -9,15 +9,18 @@ var enemies = [];
 var playerSpawnPoints = [];
 var clients = [];
 
-app.get('/', function(req, res) {
-	res.send('Server is running');
-});
+
 
 var server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080;
 var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
  
 server.listen(server_port, server_ip_address, function () {
   console.log( "Listening on " + server_ip_address + ", port " + server_port );
+});
+
+
+app.get('/', function(req, res) {
+	res.send('Server is running');
 });
 
 io.on('connection', function(socket) {
